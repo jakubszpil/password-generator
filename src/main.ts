6c5efa24b59de89160279763059279a2db27c606
Jakub Szpil <kuba0122@gmail.com>
2026-06-04 20:22:36 +0200

    * small refactor

--- src/main.ts
+++ src/main.ts
@@ -5,26 +5,66 @@ const result = document.querySelector<HTMLFieldSetElement>("#result")!;
 const clipboard = document.querySelector<HTMLFormElement>("#clipboard")!;
 const field = document.querySelector<HTMLInputElement>("#password")!;
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  generatePassword(form);
-});
+function getCharacters({
+  withSpecialCharacters,
+  withUppercaseLetters,
+  withLowercaseLetters,
+  withNumbers,
+}: {
+  withSpecialCharacters: boolean;
+  withUppercaseLetters: boolean;
+  withLowercaseLetters: boolean;
+  withNumbers: boolean;
+}): string {
+  const characters: string[] = [];
 
-clipboard.addEventListener("submit", async (event) => {
-  event.preventDefault();
-  await navigator.clipboard.writeText(field.value);
-  alert(`
-    Copied to clipboard: \r
-    ${field.value}  
-  `);
-});
+  const anyOptionSelected =
+    withSpecialCharacters ||
+    withUppercaseLetters ||
+    withLowercaseLetters ||
+    withNumbers;
 
-form.addEventListener("reset", () => {
-  result.disabled = true;
-  field.value = "";
-});
+  if (withSpecialCharacters || !anyOptionSelected) {
+    characters.push("`!#$%&'()*+,-./{|}~");
+  }
 
-function generatePassword(form: HTMLFormElement) {
+  if (withUppercaseLetters || !anyOptionSelected) {
+    characters.push("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+  }
+
+  if (withLowercaseLetters || !anyOptionSelected) {
+    characters.push("abcdefghijklmnopqrstuvwxyz");
+  }
+
+  if (withNumbers || !anyOptionSelected) {
+    characters.push("0123456789");
+  }
+
+  return characters.join("");
+}
+
+function generatePassword({
+  length,
+  characters,
+}: {
+  length: number;
+  characters: string;
+}): string {
+  let password = "";
+
+  for (let i = 0; i < length; i++) {
+    password += characters[Math.floor(Math.random() * characters.length)];
+  }
+
+  return password;
+}
+
+function setField(value: string, disabled: boolean) {
+  result.disabled = disabled;
+  field.value = value;
+}
+
+function handleSubmit(form: HTMLFormElement) {
   const values = new FormData(form);
 
   const boolean = (value: FormDataEntryValue | null) => Boolean(value) || false;
@@ -34,39 +74,29 @@ function generatePassword(form: HTMLFormElement) {
   const withUppercaseLetters = boolean(values.get("withUppercaseLetters"));
   const withLowercaseLetters = boolean(values.get("withLowercaseLetters"));
   const withNumbers = boolean(values.get("withNumbers"));
-  const length = number(values.get("length"));
+  const length = number(values.get("length")) || 36;
 
-  const validLength = length || 36;
+  const characters = getCharacters({
+    withLowercaseLetters,
+    withNumbers,
+    withSpecialCharacters,
+    withUppercaseLetters,
+  });
 
-  let characters: string = "";
-  const anyOptionSelected =
-    withSpecialCharacters ||
-    withUppercaseLetters ||
-    withLowercaseLetters ||
-    withNumbers;
+  const password = generatePassword({ length, characters });
 
-  if (withSpecialCharacters || !anyOptionSelected) {
-    characters += "`!#$%&'()*+,-./{|}~";
-  }
-
-  if (withUppercaseLetters || !anyOptionSelected) {
-    characters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  }
-
-  if (withLowercaseLetters || !anyOptionSelected) {
-    characters += "abcdefghijklmnopqrstuvwxyz";
-  }
-
-  if (withNumbers || !anyOptionSelected) {
-    characters += "0123456789";
-  }
-
-  let password = "";
-
-  for (let i = 0; i < validLength; i++) {
-    password += characters[Math.floor(Math.random() * characters.length)];
-  }
-
-  field.value = password;
-  result.disabled = false;
+  setField(password, false);
 }
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  handleSubmit(form);
+});
+
+clipboard.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  await navigator.clipboard.writeText(field.value);
+  alert("Copied to clipboard");
+});
+
+form.addEventListener("reset", () => setField("", true));
